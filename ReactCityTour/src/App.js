74a7f06ba57761641117d74e3812e2850fa4b484
naomi505 <74443287+naomi505@users.noreply.github.com>
2021-01-12 22:49:03 -0500

    Add files via upload

---
+++ ReactCityTour/src/App.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react'
+
+
+class App extends Component {
+  render () {
+    return (
+    <main>
+      <Navbar/>
+      <TourList/>
+    </main>
+    );
+  }
+}
+export default App;
